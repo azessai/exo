8515ed6e9866299bbe643604776c4f963e033b59
ES SAI <azeddine.essai@gmail.com>
2017-03-20 15:45:54 +0100

    add: angular

---
+++ app/app.js
@@ -0,0 +1,26 @@
+(function() {
+	'use strict';
+	var app = angular.module('main', []);
+
+	app.directive('orsHeader', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'tmpl/ors-header.html',
+			transclude: true
+		};
+	});
+	app.directive('orsBody', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'tmpl/ors-body.html',
+			transclude: true
+		};
+	});
+	app.directive('orsFooter', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'tmpl/ors-footer.html',
+			transclude: true
+		};
+	});
+})();
--- app/hello.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-console.log('hello AE !!!');
-for (var index = 0; index < 2; index++) {
-    a = index;
-}
