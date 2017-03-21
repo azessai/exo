c26b738201d98c95c0e5004675701c2602f9e682
ES SAI <azeddine.essai@gmail.com>
2017-03-21 17:17:50 +0100

    add: note obligatoire

--- app/ors-star.js
+++ app/ors-star.js
@@ -3,32 +3,37 @@
 
 	var app = angular.module('ors-star', ['ors-route']);
 
-	app.directive('orsStar', function() {
-		return {
-			scope: {
-				n: '=note',
-			},
-			link: function(scope, element, attrs) {
-				console.log('orsStar link', arguments);
-				scope.$watch('n', function() {
-					var html = '';
-					var note = 4;
-					note = (scope.n === undefined) ? note : Number(scope.n);
-					note = (isNaN(note)) ? 0 : note ;
-					note = (note>5) ? 5 : note;
-					note = (note<0) ? 0 : note;
-					for (let index = 0; index < note; index++) {
-						html += '<img src="./ors-star/img/yellow_star.png" />';
+	app.directive('orsStar', ['$compile', function($compile) {
+			return {
+				scope: {
+					n: '=?note',
+				},
+				link: function(scope, element, attrs) {
+					console.log('orsStar link', arguments);
+					scope.update = function(n) {
+						console.log('update', arguments);
+						scope.n = n;
 					}
-					for (let index = note; index < 5; index++) {
-						html += '<img src="./ors-star/img/white_star.png" />';
-					}
-					element.html(html);
-				});
-
+					scope.$watch('n', function() {
+						var html = '';
+						var note = 4;
+						note = (scope.n === undefined) ? note : Number(scope.n);
+						note = (isNaN(note)) ? 0 : note;
+						note = (note > 5) ? 5 : note;
+						note = (note < 0) ? 0 : note;
+						for (let index = 0; index < note; index++) {
+							html += '<img ng-click="update(' + (index + 1) + ')" src="./ors-star/img/yellow_star.png" />';
+						}
+						for (let index = note; index < 5; index++) {
+							html += '<img ng-click="update(' + (index + 1) + ')" src="./ors-star/img/white_star.png" />';
+						}
+						element.html(html);
+						$compile(element.contents())(scope);
+					});
 			}
+
 		};
-	});
+	}]);
 
 
 })();
