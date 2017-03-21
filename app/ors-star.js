553b484073a4a95bf8abece9cfc484f45fab95cf
ES SAI <azeddine.essai@gmail.com>
2017-03-21 17:06:14 +0100

    add: note

--- app/ors-star.js
+++ app/ors-star.js
@@ -5,17 +5,27 @@
 
 	app.directive('orsStar', function() {
 		return {
+			scope: {
+				n: '=note',
+			},
 			link: function(scope, element, attrs) {
 				console.log('orsStar link', arguments);
-				var html = '';
-				var note = 4
-				for (let index = 0; index < note; index++) {
-					html += '<img src="./ors-star/img/yellow_star.png" />';
-				}
-				for (let index = note; index < 5; index++) {
-					html += '<img src="./ors-star/img/white_star.png" />';
-				}
-				element.html(html);
+				scope.$watch('n', function() {
+					var html = '';
+					var note = 4;
+					note = (scope.n === undefined) ? note : Number(scope.n);
+					note = (isNaN(note)) ? 0 : note ;
+					note = (note>5) ? 5 : note;
+					note = (note<0) ? 0 : note;
+					for (let index = 0; index < note; index++) {
+						html += '<img src="./ors-star/img/yellow_star.png" />';
+					}
+					for (let index = note; index < 5; index++) {
+						html += '<img src="./ors-star/img/white_star.png" />';
+					}
+					element.html(html);
+				});
+
 			}
 		};
 	});
