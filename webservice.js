9289883c87bda347e66dcb0ddad00b8e069b3224
ES SAI <azeddine.essai@gmail.com>
2017-03-22 11:50:53 +0100

    add: product

---
+++ webservice.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var express = require('express'); // charge ExpressJS
+var fs = require('fs');
+
+var router = express.Router();
+module.exports = router;
+
+router.get(['/s1', '/s2', '/s3', '/s4', '/s5'], function(req, res, next) {
+	console.log('req.url', req.url);
+	fs.readFile('ws' + req.url, function(err, data) {
+		if (err) {
+			return console.log('error', err);
+		}
+
+		var json = JSON.parse(data);
+		var delay = json.delay;
+		console.log(json);
+		setTimeout(function() {
+			res.json(json);
+			//next();
+		}, delay);
+	});
+});
